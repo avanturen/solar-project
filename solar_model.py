aae97975f36336af75c8192a061d9e7344144b6b
avanturen <yurikof.nikita@yandex.ru>
2022-11-11 11:27:21 +0300

    some push

--- solar_model.py
+++ solar_model.py
@@ -20,8 +20,8 @@ def calculate_force(body, space_objects):
             continue  # тело не действует гравитационной силой на само себя!
         else:
             r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-            body.Fx += (gravitational_constant * body.m * obj.m / (r ** 3)) * ((body.x - obj.y))
-            body.Fy += (gravitational_constant * body.m * obj.m / (r ** 3)) * ((body.y - obj.x))
+            body.Fx += (gravitational_constant * body.m * obj.m / (r ** 3)) * ((obj.x - body.x))
+            body.Fy += (gravitational_constant * body.m * obj.m / (r ** 3)) * ((obj.y - body.y))
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
