249f550cf71526925b17b8c21b6d476fd3d576e5
mriojik <kuznetsov.ivan.n@gmail.com>
2022-11-11 10:30:09 +0300

    вычисляю силочки в функциочке calculate force

--- solar_model.py
+++ solar_model.py
@@ -18,10 +18,10 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
-
+        else:
+            r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+            body.Fx += (gravitational_constant*body.m*obj.m/(r*r))*((body.y - obj.y)/r)
+            body.Fy += (gravitational_constant*body.m*obj.m/(r*r))*((body.x - obj.x)/r)
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
